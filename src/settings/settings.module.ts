1dee698681e6d6afff6a4321f81d8de946f44cf8
ccpixel2print-lgtm <cc.pixel2print@gmail.com>
2026-08-13 13:33:28 +0000

    Fix: Settings module code issue

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
+import { JwtGuard } from '../auth/jwt.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Module({
-  providers: [SettingsService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
+  providers: [SettingsService, JwtGuard, RolesGuard],
   controllers: [SettingsController],
   exports: [SettingsService],
 })
